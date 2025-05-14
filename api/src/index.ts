4c32a261cc7cfcc72933540173632be9410522f2
eDenxGT <edenxgt@gmail.com>
2025-05-14 21:44:31 +0530

    Let's Do BW1 Task

---
+++ api/src/index.ts
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import express, { Request, Response } from "express";
+
+import { connectDB } from "./config/db";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+// import authRoutes from "./routes/auth.route";
+// import imageRoutes from "./routes/image.route";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ALLOWED_ORIGIN,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/img", imageRoutes);
+
+app.use("/", (req: Request, res: Response) => {
+  res.json("Application is running!!!");
+});
+
+app.use(errorHandler);
+
+connectDB();
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
